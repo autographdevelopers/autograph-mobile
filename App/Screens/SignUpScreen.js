6ff4416e75cc067695f7703ee516f28d296a7915
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2017-11-27 16:05:06 +0100

    added button to trigger datepicker

--- App/Screens/SignUpScreen.js
+++ App/Screens/SignUpScreen.js
@@ -1,13 +1,13 @@
-import { Text, View, ScrollView } from 'react-native';
+import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import React, { Component } from 'react';
 import NavHeader from '../Components/NavHeader';
-import {reduxForm, Field } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 import RadioButton from '../Components/RadioButton';
 import InputField from '../Components/InputField';
 import CheckBox from '../Components/CheckBox';
 import InputLabel from '../Components/InputLabel';
-import {StyleSheet} from 'react-native';
-import {Fonts, Colors} from '../Themes/';
+import { StyleSheet } from 'react-native';
+import { Fonts, Colors } from '../Themes/';
 import PrimaryButton from '../Components/ButtonPrimary';
 import Layout from '../Components/Layout';
 import DatePicker from 'react-native-datepicker';
@@ -37,89 +37,123 @@ const styles = StyleSheet.create({
     color: Colors.strongGrey,
     marginLeft: 20
   },
-  wrapper: {
+  btnWrapper: {
     flex: 1,
     alignItems: 'center',
-    marginTop: 43
+    marginTop: 30
   },
   datepicker: {
-    marginBottom: 15,
+    flex: 1,
+    alignItems: 'center',
   },
+  datepickerRow: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+    marginTop: 10
+  },
+  datepickerLabel: {
+    fontSize: Fonts.size.medium,
+    color: Colors.primaryWarm
+  }
 });
 
 const datepickerCustom = {
   dateInput: {
     borderWidth: 0,
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
+    height: Fonts.size.medium
   },
   dateText: {
     textAlign: 'left',
     fontSize: Fonts.size.medium
+  },
+  dateTouchBody: {
+    height: Fonts.size.medium
+  },
+  btnTextConfirm: {
+    color: Colors.primaryWarm
   }
 };
 
 class SignUpScreen extends Component {
   static navigationOptions = {
-    header: (props) => <NavHeader navigation={props.navigation} title={'Załóż konto'} />
+    header: (props) => <NavHeader navigation={props.navigation} title={'Załóż konto'}/>
   };
 
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.state = {date:"2016-05-15"};
+    this.state = { date: "2016-05-15" };
   }
 
-  render () {
+  render() {
     return (
       <Layout>
         <InputLabel label={'Kim jestes?'} required={true}/>
-        <View style={styles.selectRow} >
+        <View style={styles.selectRow}>
           <View style={styles.selectOption}>
-            <Field name={'role'} boundValue={'employee'} setValue={ ()=> this.props.change('role', 'employee') } component={RadioButton} />
+            <Field name={'role'} boundValue={'employee'} setValue={() => this.props.change('role', 'employee')}
+                   component={RadioButton}/>
             <Text style={styles.selectLabel}>Pracownik</Text>
           </View>
           <View style={styles.selectOption}>
-            <Field name={'role'} boundValue={'student'} setValue={ ()=> this.props.change('role', 'student') } component={RadioButton} />
+            <Field name={'role'} boundValue={'student'} setValue={() => this.props.change('role', 'student')}
+                   component={RadioButton}/>
             <Text style={styles.selectLabel}>Kursant</Text>
           </View>
         </View>
 
-        <Field name={'email'} component={InputField} label={'Email'} required={true} />
+        <Field name={'email'} component={InputField} label={'Email'} required={true}/>
         <Field name={'firstName'} component={InputField} label={'Imię'} required={true}/>
         <Field name={'lastName'} component={InputField} label={'Nazwisko'} required={true}/>
-        <Field name={'password'} component={InputField} label={'Haslo'} required={true} options={{secureTextEntry: true}} />
-        <Field name={'passwordConfirmation'} component={InputField} label={'Potwierdź haslo '} required={true} options={{secureTextEntry: true}} />
+        <Field name={'password'} component={InputField} label={'Haslo'} required={true}
+               options={{ secureTextEntry: true }}/>
+        <Field name={'passwordConfirmation'} component={InputField} label={'Potwierdź haslo '} required={true}
+               options={{ secureTextEntry: true }}/>
 
         <InputLabel label={'Plec'}/>
-        <View style={styles.selectRow} >
+        <View style={styles.selectRow}>
           <View style={styles.selectOption}>
-            <Field name={'gender'} boundValue={'male'} setValue={ ()=> this.props.change('gender', 'male') } component={RadioButton} />
+            <Field name={'gender'} boundValue={'male'} setValue={() => this.props.change('gender', 'male')}
+                   component={RadioButton}/>
             <Text style={styles.selectLabel}>Mężczyzna</Text>
           </View>
           <View style={styles.selectOption}>
-            <Field name={'gender'} boundValue={'female'} setValue={ ()=> this.props.change('gender', 'female') } component={RadioButton} />
+            <Field name={'gender'} boundValue={'female'} setValue={() => this.props.change('gender', 'female')}
+                   component={RadioButton}/>
             <Text style={styles.selectLabel}>Kobieta</Text>
           </View>
         </View>
 
         <InputLabel label={'Data urodzenia'} required={true}/>
-        <DatePicker
-          style={styles.datepicker}
-          customStyles={datepickerCustom}
-          date={this.state.date}
-          showIcon={false}
-          mode="date"
-          placeholder="Kliknij aby prowadzic date"
-          format="YYYY-MM-DD"
-          confirmBtnText="Potwierdz"
-          cancelBtnText="Anuluj"
-          onDateChange={(date) => {this.setState({date: date})}}
-        />
+        <View style={styles.datepickerRow}>
+          <DatePicker
+            style={styles.datepicker}
+            customStyles={datepickerCustom}
+            ref={(picker) => { this.datePicker = picker; }}
+            date={this.state.date}
+            showIcon={false}
+            mode="date"
+            placeholder="Kliknij aby prowadzic date"
+            format="YYYY-MM-DD"
+            confirmBtnText="Potwierdz"
+            cancelBtnText="Anuluj"
+            onDateChange={(date) => {
+              this.setState({ date: date })
+            }}
+          />
+          <TouchableOpacity style={{flex:1}} onPress={()=>this.datePicker.onPressDate()}>
+            <Text style={styles.datepickerLabel}>Wybierz datę</Text>
+          </TouchableOpacity>
+        </View>
 
         <View style={styles.acceptTermsRow}>
-          <Field name={'accepted'} setValue={ (value)=> () => this.props.change('accepted', value) } component={CheckBox} />
+          <Field name={'accepted'} setValue={(value) => () => this.props.change('accepted', value)}
+                 component={CheckBox}/>
           <Text style={styles.acceptTermsLabel}>Zgadzam się na zasady i warunki serwisu AutoGraph.</Text>
         </View>
-        <View style={styles.wrapper}>
+        <View style={styles.btnWrapper}>
           <PrimaryButton>Zarejestruj</PrimaryButton>
         </View>
       </Layout>
@@ -127,4 +161,4 @@ class SignUpScreen extends Component {
   }
 }
 
-export default reduxForm({form: 'signUp', initialValues: { accepted: false, gender: 'male' } })(SignUpScreen);
+export default reduxForm({ form: 'signUp', initialValues: { accepted: false, gender: 'male' } })(SignUpScreen);
