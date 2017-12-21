c6e41ad8dcbdd7f9427ddc6b6d9c490331d8d2e4
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2017-12-22 00:46:15 +0100

    fixed bug, added validations and done refactro on schedult boundaries view

--- App/Components/ScheduleBoundariesView.js
+++ App/Components/ScheduleBoundariesView.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity, View, TextInput, ScrollView } from 'react-native';
 import { StyleSheet } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import moment from 'moment';
 
 import { Fonts, Colors, Metrics } from '../Themes/';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import CheckBox from './CheckBox';
 import ButtonText from './ButtonText';
-import moment from 'moment';
-import {roundTimeToHalfHourInterval} from '../Lib/timeHandlers';
+import { roundTimeToHalfHourInterval } from '../Lib/timeHandlers';
 
 const styles = StyleSheet.create({
   title: {
@@ -48,17 +48,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center'
   },
-  weekdaysColumn: {
-    marginRight: 20
-  },
   datepicker: {
     height: 0,
     opacity: 0,
-    borderWidth: 0,
+    borderWidth: 0
   },
-  weekdayItem: {
-    marginVertical: 10,
-    marginLeft: 15
+  weekdayInfo: {
+    justifyContent: 'center',
+    flex: 1,
+    marginVertical: 5
+  },
+  weekdayLabelContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  weekdayLabel: {
+    marginHorizontal: 10,
+    marginVertical: 10
   },
   applyToAllDays: {
     marginVertical: 10
@@ -93,24 +100,12 @@ export default class ScheduleBoundariesView extends Component {
   };
 
   setTime = time => {
-    const newWeekdays = [].concat(this.props.value),
-    timeArray = time.split(':'),
-    m = timeArray[1],
-    h = timeArray[0];
-    console.log(time);
-    console.log(m);
-    console.log(h);
+    const newWeekdays = [].concat(this.props.value), // deep or shallow copy?
+      timeArray = time.split(':'),
+      m = timeArray[1],
+      h = timeArray[0];
 
-    const {hour, minutes} = roundTimeToHalfHourInterval(h, m);
-
-    console.log('*****');
-    console.log('helper result:');
-    console.log(roundTimeToHalfHourInterval(h, m));
-    console.log('hour');
-    console.log(hour);
-    console.log('minutes');
-    console.log(minutes);
-
+    const { hour, minutes } = roundTimeToHalfHourInterval(h, m);
     const datetime = moment();
 
     datetime.hours(hour);
@@ -124,6 +119,7 @@ export default class ScheduleBoundariesView extends Component {
     this.setState({
       startend: startend
     });
+
     this.datePicker.onPressDate();
   };
 
@@ -149,14 +145,14 @@ export default class ScheduleBoundariesView extends Component {
     this.props.setValue(newWeekdays);
   };
 
-  currentStartEndTime = (startend, value) => ( this.displayTime(value[this.state.currentWeekday][startend]) );
+  currentStartEndTime = (startend, value) => (this.displayTime(value[this.state.currentWeekday][startend]));
 
   displayTime = datetime => (
     datetime ? datetime.format(TIME_FORMAT) : '-:-'
   );
 
   render() {
-    const {value} = this.props;
+    const { value } = this.props;
 
     return (
       <ScrollView>
@@ -181,40 +177,44 @@ export default class ScheduleBoundariesView extends Component {
         </View>
 
         <View>
-          <ButtonText onPress={this.applyToAllDays} customStyle={styles.applyToAllDays} position={'flex-start'}>Zastosuj dla kazego dnia</ButtonText>
+          <ButtonText onPress={this.applyToAllDays} customStyle={styles.applyToAllDays} position={'flex-start'}>Zastosuj
+            dla kazego dnia</ButtonText>
         </View>
-        <View style={styles.row}>
-          <View style={styles.weekdaysColumn}>
-            {
-              WEEKDAYS.map((element, index) => (
-                <View style={styles.dayRow} key={`day-${index}`}>
-                  <CheckBox value={this.isIntervalSet(index)} setValue={this.clearDay(index)}/>
-                  <Text style={styles.weekdayItem}>{element}</Text>
-                </View>)
-              )
-            }
-          </View>
-          <View>
-            {
-              WEEKDAYS.map((_, index) => {
-                const { start_time, end_time } =  value[index];
-                return (<Text key={`interval-${index}`} style={styles.weekdayItem}>{this.displayTime(start_time)}   -   {this.displayTime(end_time)}</Text>)
-              })
-            }
-          </View>
-        </View>
+        {
+          WEEKDAYS.map((element, index) => {
+            const { start_time, end_time } = value[index];
 
+            return (
+            <View style={styles.dayRow} key={`day-${index}`}>
+              <View style={styles.weekdayLabelContainer}>
+                <CheckBox value={this.isIntervalSet(index)} setValue={this.clearDay(index)}/>
+                <Text style={styles.weekdayLabel}>{element}</Text>
+              </View>
+
+              <View style={styles.weekdayInfo}>
+                <Text>
+                  {this.displayTime(start_time)} - {this.displayTime(end_time)}
+                </Text>
+                {
+                  start_time && end_time && end_time.isBefore(start_time) &&
+                  <Text style={{color: Colors.salmon}}>Koniec przed poczatkiem.</Text>
+                }
+              </View>
+            </View>
+            )
+          })
+        }
         <DatePicker
           style={styles.datepicker}
           ref={picker => this.datePicker = picker}
-          date={this.displayTime(value[this.state.currentWeekday][this.state.startend])}
+          date={value[this.state.currentWeekday][this.state.startend]}
           showIcon={false}
           mode='time'
           format={TIME_FORMAT}
           minuteInterval={30}
           confirmBtnText='Potwierdz'
           cancelBtnText='Anuluj'
-          onDateChange={ date => this.setTime(date) }
+          onDateChange={date => this.setTime(date)}
         />
       </ScrollView>
     );
