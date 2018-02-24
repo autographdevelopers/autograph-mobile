4552c97407b720de50be1af480bd067b4165e74c
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2018-02-24 13:38:01 +0100

    initial version ready

--- App/Components/DialogBox.js
+++ App/Components/DialogBox.js
@@ -10,6 +10,7 @@ import { StyleSheet } from 'react-native';
 import { Colors, Fonts } from '../Themes/index';
 import Icon from 'react-native-vector-icons/Ionicons';
 import ButtonPrimary from './ButtonPrimary';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
 
 // Props API
 // dialogText, successTexts, failureTexts - { title, description }
@@ -21,9 +22,9 @@ export default DialogBox = ({
                               successTexts,
                               failureTexts,
                               status,
-                              loaderLabel='Ładowanie..',
+                              loaderLabel = 'Ładowanie..',
                               children,
-                              mode,
+                              mode='warning',
                               icon,
                               dialogBtn,
                               successBtn,
@@ -32,8 +33,15 @@ export default DialogBox = ({
                               modalProps = {},
                             }) => {
 
+  const CIRCLE_SIZE = 450;
+  const CIRCLE_TOP_OFFSET = -330;
+  const BREATH_SPACE = 15;
+  const TOP_OF_THE_STACK = 999999999999;
+  const MODAL_SPINNER_MODE_SIZE = 300;
+
   const styles = StyleSheet.create({
     modalContainer: {
+      /** OVERLAY */
       flex: 1,
       backgroundColor: 'rgba(0,0,0, .8)',
       justifyContent: 'center',
@@ -42,33 +50,22 @@ export default DialogBox = ({
     innerContainer: {
       borderRadius: 20,
       overflow: 'hidden',
-      height: 400,
-      width: 300,
-      alignSelf: 'center',
       backgroundColor: Colors.snow,
     },
-    rectangle: {
-      height: 70,
-      flexDirection: 'row', // gives 100% width occupied
-      backgroundColor: 'red',
-      borderTopRightRadius: 20,
-      borderTopLeftRadius: 20,
-    },
     semicircle: {
-      height: 400,
-      width: 400,
-      top: -300,
+      height: CIRCLE_SIZE,
+      width: CIRCLE_SIZE,
+      top: CIRCLE_TOP_OFFSET,
       position: 'absolute',
-      borderRadius: 200,
+      borderRadius: CIRCLE_SIZE / 2,
       backgroundColor: Colors.subtleGray,
       alignSelf: 'center',
     },
     title: {
-      marginTop: 100,
       textAlign: 'center',
       fontFamily: Fonts.type.base,
       fontWeight: '600',
-      fontSize: Fonts.size.regular,
+      fontSize: 22,
     },
     description: {
       textAlign: 'center',
@@ -76,48 +73,107 @@ export default DialogBox = ({
       fontFamily: Fonts.type.base,
       fontSize: Fonts.size.medium,
       fontWeight: '400',
-
     },
-    closeBtn: {
+    crossIconBox: {
       position: 'absolute',
       top: 15,
       right: 15,
+      zIndex: TOP_OF_THE_STACK
     },
     crossIcon: {
       backgroundColor: 'transparent',
       fontWeight: '100',
     },
     triangle: {
+      /** shape */
       width: 0,
       height: 0,
-      borderTopWidth: 400,
-      borderRightWidth: 300,
-      borderTopColor: 'transparent',
+      borderTopWidth: MODAL_SPINNER_MODE_SIZE,
+      borderRightWidth: MODAL_SPINNER_MODE_SIZE,
       borderRightColor: Colors.subtleGray,
+      borderTopColor: 'transparent',
+      /** position */
       position: 'absolute',
       bottom: 0,
       left: 0,
     },
-    spinner: {
-      alignSelf: 'center',
-      flex : 1,
+    spinnerView: {
       alignItems: 'center',
       justifyContent: 'center',
-      width: '100%',
+      height: MODAL_SPINNER_MODE_SIZE,
+      width: MODAL_SPINNER_MODE_SIZE
     },
     loaderText: {
       fontFamily: Fonts.type.base,
       fontSize: Fonts.size.medium,
       fontWeight: '300',
-      backgroundColor: 'transparent'
+      backgroundColor: 'transparent',
+    },
+    actionView: {
+      overflow: 'hidden',
+      paddingHorizontal: 40,
+      paddingTop: CIRCLE_SIZE + CIRCLE_TOP_OFFSET,
+      minHeight: CIRCLE_SIZE + CIRCLE_TOP_OFFSET + BREATH_SPACE + 100,
+      width: '80%'
+    },
+    headerIconContainer: {
+      /** container covers visible part of a circle */
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      height: CIRCLE_SIZE + CIRCLE_TOP_OFFSET,
+      backgroundColor: 'transparent',
+
+      /** for centering icon within */
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    contentContainer: {
+      justifyContent: 'space-between',
+    },
+    textArea: {
+      marginVertical: 2*BREATH_SPACE
+    },
+    actionButtonContainer: {
+      paddingVertical: 25
     }
   });
 
+  const defaultIcons = {
+    warning: <IconFontAwesome name={'warning'} color={Colors.yellowLight}
+                              size={65}/>,
+  };
+
   const LoaderView = () => (
-    <View style={styles.spinner}>
+    <View style={styles.spinnerView}>
       <View style={styles.triangle}/>
       <ActivityIndicator color={Colors.primaryWarm} size={'large'}/>
       <Text style={styles.loaderText}>{loaderLabel}</Text>
+    </View>
+  );
+
+  const ActionView = () => (
+    <View style={styles.actionView}>
+      <View style={styles.semicircle}/>
+      <TouchableOpacity style={styles.crossIconBox}>
+        <Icon name="md-close" color={Colors.softBlack} size={24}
+              style={styles.crossIcon}/>
+      </TouchableOpacity>
+      <View style={styles.headerIconContainer}>{defaultIcons['warning']}</View>
+
+      <View style={styles.contentContainer}>
+        <View style={styles.textArea}>
+          <Text style={styles.title}>Hello are you sure?</Text>
+          <Text style={styles.description}>Some interesting lorem ipsum
+            description</Text>
+        </View>
+        {children}
+        <View style={styles.actionButtonContainer}>
+        <ButtonPrimary theme={'warning'}
+                       customWrapperStyles={{ width: '100%', marginBottom: 0 }}>Yes
+          I do</ButtonPrimary>
+        </View>
+      </View>
     </View>
   );
 
@@ -129,15 +185,8 @@ export default DialogBox = ({
     >
       <View style={styles.modalContainer}>
         <View style={styles.innerContainer}>
-          <View style={styles.semicircle}/>
-          <TouchableOpacity style={styles.closeBtn}>
-          <Icon name="md-close" color={Colors.softBlack} size={24} style={styles.crossIcon}/>
-          </TouchableOpacity>
-          <Text style={styles.title}>Hello are you sure?</Text>
-          <Text style={styles.description}>Some interesting lorem ipsum
-          description</Text>
+          <ActionView/>
           {/*<LoaderView/>*/}
-          <ButtonPrimary>Zapisz</ButtonPrimary>
         </View>
       </View>
     </Modal>
--- App/Components/Stories/DialogBox.story.js
+++ App/Components/Stories/DialogBox.story.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { View } from 'react-native';
 import DialogBox from '../DialogBox';
+import { TextInput, Text, View } from 'react-native';
 
-storiesOf('DialogBox').add('Default', () => (
+storiesOf('DialogBox')
+// .add('Default', () => (
+//   <View style={{flex:1, backgroundColor: 'grey'}}>
+//     <DialogBox visible={true} />
+//   </View>
+// )).add('Loading mode', () => (
+//   <View style={{flex:1, backgroundColor: 'grey'}}>
+//     <DialogBox visible={true} />
+//   </View>
+// ))
+.add('W/ children', () => (
   <View style={{flex:1, backgroundColor: 'grey'}}>
-    <DialogBox visible={true} />
+    <DialogBox visible={true} >
+    <View>
+      <Text>Lorem ipsum dolor...</Text>
+      <TextInput/>
+      <Text>Lorem ipsum dolor...</Text>
+      <TextInput/>
+      <Text>Lorem ipsum dolor...</Text>
+      <TextInput/>
+    </View>
+    </DialogBox>
   </View>
-));
+))
+
+
 
