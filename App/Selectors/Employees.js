60ea9baceac01606699ba9cfe8aaf980d184e37d
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2018-05-14 14:52:42 +0200

    Remove yourself from the list of employees

--- App/Selectors/Employees.js
+++ App/Selectors/Employees.js
@@ -1,3 +1,6 @@
+const getCurrentUser = state => state.access.currentUser;
+const notMe = state => employee => employee.id !== getCurrentUser(state).id;
+
 export const getCurrentEmployee = state => {
   const id = state.support.context.currentEmployeeID;
 
@@ -6,10 +9,13 @@ export const getCurrentEmployee = state => {
 
 export const getPendingEmployees = state => {
   return state.entities.employees.pendingIds
-       .map(id => state.entities.employees.pending[id]);
+       .map(id => state.entities.employees.pending[id])
+              .filter(notMe(state))
+
 };
 
 export const getActiveEmployees = state => {
   return state.entities.employees.activeIds
        .map(id => state.entities.employees.active[id])
+              .filter(notMe(state))
 };
